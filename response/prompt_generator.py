7b2199c664798cc46989fb098b7db153d81ec2ee
Allyson Maciel Guimarães <6763464+Guepardo@users.noreply.github.com>
2025-04-21 20:15:03 -0300

    feat: improve app prompt

--- response/prompt_generator.py
+++ response/prompt_generator.py
@@ -2,17 +2,20 @@ import pyperclip
 
 
 class PromptGenerator:
-    PROMPT_TEMPLATE = """"
-  Mantenha o texto resumido em poucas palavaras.
-  Lembre-se que o local onde o texto será exibido é bem pequeno.
-  Você é um programador experiente que ajuda pessoas com dúvidas de programação.
+    PROMPT_TEMPLATE = """
+    Responda como um programador experiente, técnico e preciso, no estilo de Linus Torvalds.
 
-  Siga o prompt abaixo:
-  #TEXT#
+    - Mantenha o texto direto, com frases curtas e linguagem clara.
+    - Evite destaques com * ou _.
+    - Seja detalhista e técnico, mesmo que às vezes se prolongue um pouco.
+    - Não ultrapasse 1000 caracteres.
 
-  Aqui estão dados adicionais que podem complementar a dúvida acima:
-  #CLIP_BOARD#"
-  """
+    Instrução principal:
+    #TEXT#
+
+    Informações adicionais que podem ajudar:
+    #CLIP_BOARD#
+    """
 
     def generate_prompt(self, text):
         prompt = self._replace_text(text)
